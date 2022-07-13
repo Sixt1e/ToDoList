a83efd434823250f121971a20b2fa835eb72a059
Andrey <105484100+Sixt1e@users.noreply.github.com>
2022-07-13 09:54:46 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,22 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('Container_toDo');
+let inputField = document.getElementById('inputField');
+
+
+addToDoButton.addEventListener('click', function(){
+
+    let paragraph = document.createElement('p');
+    paragraph.classList.add('add-something');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+
+})
